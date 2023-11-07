88de0364e029d9898c2af890087f6cffc607a4ac
Krishanu <krishgoswami15@gmail.com>
2023-11-07 22:30:40 +0530

    modified

--- server/dbconfig/connectDB.js
+++ server/dbconfig/connectDB.js
@@ -6,9 +6,7 @@ dotenv.config();
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            dbName: "coursesapp",
+            dbName: "alemeno",
         });
         console.log("Database connected successfully");
     } catch (error) {
