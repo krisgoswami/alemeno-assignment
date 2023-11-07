20b3f79492febee4b73a71d6c69567a14c892726
Krishanu <krishgoswami15@gmail.com>
2023-11-07 22:35:23 +0530

    controllers added

---
+++ server/controllers/adminController.js
@@ -0,0 +1,85 @@
+import jwt from 'jsonwebtoken';
+import { Admin } from '../models/adminModel.js';
+
+// *********admin registration*********
+
+export const createAdmin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        //validations
+        if (!email || !password) {
+            return res.status(400).send({
+                message: 'fill all fields',
+                success: false,
+            });
+        }
+
+        //validation for existing admin
+        const existingAdmin = await Admin.findOne({ email });
+        if (existingAdmin) {
+            return res.status(400).send({
+                message: 'admin already exists',
+                success: false
+            })
+        }
+
+        //saving admin to the database
+        const admin = new Admin({
+            email: email,
+            password: password,
+        });
+        await admin.save();
+        const token = jwt.sign({ email, role: 'admin' }, process.env.SECRET, { expiresIn: '1d' }); //created this token such that user can login upon signing up
+        return res.status(200).send({
+            message: 'Admin registered',
+            success: true,
+            admin,
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: 'Error registering admin',
+            success: false,
+            error,
+        });
+    }
+}
+
+// *********admin login*********
+
+export const adminLogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        //validations
+        if (!email || !password) {
+            return res.status(400).send({
+                message: 'fill all fields',
+                success: false,
+            });
+        }
+
+        //validation to check if credentials are correct
+        const admin = await Admin.findOne({ email, password });
+        if (!admin) {
+            return res.status(400).send({
+                message: "email or password incorrect",
+                success: false,
+            });
+        }
+        const token = jwt.sign({ email, role: "admin" }, process.env.SECRET, { expiresIn: '1d' });
+        res.status(200).send({
+            message: "Login successful",
+            success: true,
+            admin,
+            token,
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: 'Error logging in',
+            success: false,
+            error,
+        });
+    }
+}
---
+++ server/controllers/studentController.js
@@ -0,0 +1,90 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/studentModel.js';
+
+
+// *********user registration*********
+
+export const createUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        //validations
+        if (!name || !email || !password) {
+            return res.status(400).send({
+                message: "please fill all fields",
+                success: false,
+            });
+        };
+
+        //check for existing user
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send({
+                message: "User already exists",
+                success: false,
+            });
+        };
+
+        //save user to database
+        const user = new User({
+            name: name,
+            email: email,
+            password: password,
+        });
+        await user.save();
+
+        //create token upon sign up
+        const token = jwt.sign({ email, role: "user" }, process.env.SECRET, { expiresIn: '1d' });
+        return res.status(200).send({
+            message: "User registered successfully",
+            success: true,
+            user,
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: 'Error registering user',
+            success: false,
+            error,
+        });
+    }
+}
+
+// *********user login*********
+
+export const userLogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        //validations
+        if (!email || !password) {
+            return res.status(400).send({
+                message: "please fill all fields",
+                success: false,
+            });
+        };
+
+        //validation to check if credentials are correct
+        const user = await User.findOne({ email, password });
+        if (!user) {
+            return res.status(403).send({
+                message: "Email or password incorrect",
+                success: false,
+            });
+        }
+        const token = jwt.sign({ email, role: "user" }, process.env.SECRET, { expiresIn: "1d" });
+        res.status(200).send({
+            message: "successfully logged in",
+            success: true,
+            user,
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: 'Error logging in',
+            success: false,
+            error,
+        });
+    }
+}
