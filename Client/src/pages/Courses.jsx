77981c3d09d4fe83a171967797c39fe3bc421c07
Krishanu <krishgoswami15@gmail.com>
2023-11-09 17:58:59 +0530

    pagination added

--- Client/src/pages/Courses.jsx
+++ Client/src/pages/Courses.jsx
@@ -7,7 +7,8 @@ import { CoursesCard } from '../components/CourseCard';
 const Courses = () => {
 
     const [courses, setCourses] = useState([]);
-    // const token = localStorage.getItem('token');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(4); //default value of items per page
 
     const getCourses = async () => {
         try {
@@ -23,9 +24,26 @@ const Courses = () => {
         getCourses();
     }, []);
 
+    // Calculate the index of the first and last item on the current page
+    const indexOfLastCourse = currentPage * itemsPerPage;
+    const indexOfFirstCourse = indexOfLastCourse - itemsPerPage;
+    const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse);
+
+    // Change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
     return (
         <div className='flex flex-col h-full p-4 mt-20'>
-            {courses?.map((course) =>
+            <div className="mx-10 mb-4">
+                <label className="block text-gray-700 mb-2">Courses per page:</label>
+                <input
+                    type="number"
+                    value={itemsPerPage}
+                    onChange={(e) => setItemsPerPage(Number(e.target.value))}
+                    className="w-32 border border-gray-300 rounded px-4 py-2"
+                />
+            </div>
+            {currentCourses?.map((course) =>
                 <div key={course?._id} className="mx-10 mt-4">
                     <CoursesCard
                         id={course?._id}
@@ -38,6 +56,23 @@ const Courses = () => {
                     />
                 </div>
             )}
+            {/* Pagination controls */}
+            <div className="mt-4 mx-10">
+                <button
+                    onClick={() => paginate(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    className="mr-2 px-4 py-2 border border-gray-300 rounded"
+                >
+                    Previous
+                </button>
+                <button
+                    onClick={() => paginate(currentPage + 1)}
+                    disabled={indexOfLastCourse >= courses.length}
+                    className="px-4 py-2 border border-gray-300 rounded"
+                >
+                    Next
+                </button>
+            </div>
         </div>
     )
 }
