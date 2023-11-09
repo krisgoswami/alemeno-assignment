247b425091fa56ff3d734ec397cb9400b92e9ff8
Krishanu <krishgoswami15@gmail.com>
2023-11-09 20:12:33 +0530

    added dashboard

---
+++ Client/src/pages/EnrolledCourses.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { BASE_URL } from '../utils/helper';
+import { DashboardCard } from '../components/CourseCard';
+
+const EnrolledCourses = () => {
+
+    const [purchasedCourses, setPurchasedCourses] = useState([]);
+    const token = localStorage.getItem('token');
+    const email = localStorage.getItem('email');
+
+    const getPurchasedCourses = async () => {
+        try {
+            const { data } = await axios.get(`${BASE_URL}/api/v1/user/enrolledCourses`, {
+                headers: {
+                    'email': email,
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+            console.log(data);
+            if (data.success) {
+                setPurchasedCourses(data.enrolledCourses);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getPurchasedCourses();
+    }, [])
+
+
+    return (
+        <div className="w-full h-screen p-5 pb-10 mt-20">
+            <h2 className="text-xl font-bold mb-10 ml-10">Dashboard</h2>
+            <div className='flex justify-start'>
+                {purchasedCourses?.map((course) =>
+                    <div key={course?._id} className="mx-10 mt-4">
+                        <DashboardCard
+                            id={course?._id}
+                            title={course?.title}
+                            instructor={course?.instructor}
+                            thumbnail={course?.thumbnail}
+                        />
+                    </div>
+                )}
+                {purchasedCourses.length === 0 &&
+                    <p className="text-xl font-medium mx-10 mb-4">You have not made any purchases yet</p>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default EnrolledCourses;
