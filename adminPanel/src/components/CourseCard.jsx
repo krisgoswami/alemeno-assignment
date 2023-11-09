f74f19151619473d8eb12e96064bef8c503e26f3
Krishanu <krishgoswami15@gmail.com>
2023-11-09 13:28:45 +0530

    mod

--- adminPanel/src/components/CourseCard.jsx
+++ adminPanel/src/components/CourseCard.jsx
@@ -6,7 +6,7 @@ const CourseCard = ({ id, title, description, price, thumbnail }) => {
     const navigate = useNavigate();
 
     return (
-        <div className="bg-cyan-200 w-[350px] max-h-[450px] h-[500px] p-4 shadow-md rounded-2xl">
+        <div className="bg-cyan-200 w-80 max-h-[450px] h-[500px] p-4 shadow-md rounded-2xl">
             <img src={thumbnail} alt={''} className="w-full h-40 object-cover mb-4 rounded-md" />
             <h3 className="text-xl text-black font-medium mb-2 truncate max-h-24 overflow-hidden">{title}</h3>
             <p className="text-gray-700 mb-2 font-semibold ">{description}</p>
