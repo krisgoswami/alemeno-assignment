dd6a91ec2fa24380244b1bc960431c967319a3d3
Krishanu <krishgoswami15@gmail.com>
2023-11-10 10:07:01 +0530

    added user profile routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { completedCourse, completedCourses, createUser, enrollCourse, enrolledCourses, getCourse, getCourses, searchCourses, userLogin } from '../controllers/studentController.js';
+import { completedCourse, completedCourses, createUser, enrollCourse, enrolledCourses, getCourse, getCourses, searchCourses, userDetails, userLogin, userProfile } from '../controllers/studentController.js';
 import { authenticateJwt } from '../utils/jwtAuth.js';
 
 const router = express.Router();
 
 router.post('/signup', createUser);
 router.post('/login', userLogin);
+router.get('/profile/:id', authenticateJwt, userDetails);
+router.put('/profile/:id', authenticateJwt, userProfile);
 router.get('/courses', getCourses);
 router.get('/course/:id', getCourse);
 router.get('/search', searchCourses);
