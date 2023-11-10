3b11b544238a32a81ea2ca1f2a9d4a0bb38e447f
Krishanu <krishgoswami15@gmail.com>
2023-11-10 20:19:19 +0530

    added comments

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -4,12 +4,12 @@ import { authenticateJwt } from '../utils/jwtAuth.js';
 
 const router = express.Router();
 
-router.post('/signup', createAdmin);
-router.post('/login', adminLogin);
-router.post('/createCourse', authenticateJwt, createCourse);
-router.get('/getCourses', authenticateJwt, getCourses);
-router.get('/getCourse/:id', authenticateJwt, getCourse);
-router.put('/update/:id', authenticateJwt, updateCourse);
-router.delete('/delete/:id', authenticateJwt, deleteCourse);
+router.post('/signup', createAdmin); //admin signup
+router.post('/login', adminLogin); //admin login
+router.post('/createCourse', authenticateJwt, createCourse); //create course
+router.get('/getCourses', authenticateJwt, getCourses); //get all courses
+router.get('/getCourse/:id', authenticateJwt, getCourse); // get course by id
+router.put('/update/:id', authenticateJwt, updateCourse); //update an existing course
+router.delete('/delete/:id', authenticateJwt, deleteCourse); //delete a course
 
 export default router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,16 +4,16 @@ import { authenticateJwt } from '../utils/jwtAuth.js';
 
 const router = express.Router();
 
-router.post('/signup', createUser);
-router.post('/login', userLogin);
-router.get('/profile/:id', authenticateJwt, userDetails);
-router.put('/profile/:id', authenticateJwt, userProfile);
-router.get('/courses', getCourses);
-router.get('/course/:id', getCourse);
-router.get('/search', searchCourses);
-router.post('/enroll/:id', authenticateJwt, enrollCourse);
-router.get('/enrolledCourses', authenticateJwt, enrolledCourses);
-router.get('/completedCourses', authenticateJwt, completedCourse);
-router.post('/markComplete/:id', authenticateJwt, completedCourses);
+router.post('/signup', createUser); //user registration
+router.post('/login', userLogin); //user login
+router.get('/profile/:id', authenticateJwt, userDetails); //get user details
+router.put('/profile/:id', authenticateJwt, userProfile); //update user details
+router.get('/courses', getCourses); //get courses
+router.get('/course/:id', getCourse); //get course by id
+router.get('/search', searchCourses); //search for courses
+router.post('/enroll/:id', authenticateJwt, enrollCourse); //enroll for a course
+router.get('/enrolledCourses', authenticateJwt, enrolledCourses); //get enrolled courses
+router.get('/completedCourses', authenticateJwt, completedCourse); //get courses that are marked as completed
+router.post('/markComplete/:id', authenticateJwt, completedCourses); //mark a course as completed
 
 export default router;
