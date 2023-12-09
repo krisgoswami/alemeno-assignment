110bb96f7ac1eaa38c71394ea6a392a752bd8c52
Krishanu <krishgoswami15@gmail.com>
2023-12-09 22:38:27 +0530

    updated cart page

--- Client/src/pages/Cart.jsx
+++ Client/src/pages/Cart.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { CartCard } from '../components/CourseCard';
 
 const Cart = () => {
+
+    const dispatch = useDispatch();
+    const cartItems = useSelector((state) => state.cart);
     return (
-        <div>Cart</div>
+        <div>
+            {cartItems.map((item) =>
+                <div key={item?._id} className="mx-4 md:mx-10 mt-4">
+                    <CartCard
+                        id={item.id}
+                        title={item?.title}
+                        instructor={item?.instructor}
+                        price={item?.price}
+                        thumbnail={item?.thumbnail}
+                    />
+                </div>
+            )}
+        </div>
     )
 }
 
-export default Cart
+export default Cart;
